056198f7c81c6586b5383d6245badae0eb59f146
blajder <blajder123@gmail.com>
2019-08-06 09:06:34 +0200

    Init

---
+++ src/main/java/com/example/demo/PoolTableBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolTableBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolTableBackendApplication.class, args);
+	}
+
+}
