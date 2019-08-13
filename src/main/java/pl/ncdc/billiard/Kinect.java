a34fcd9690395ba9de27c7c835fa7da118575802
blajder <blajder123@gmail.com>
2019-08-13 08:42:20 +0200

    Add white ball detection

--- src/main/java/pl/ncdc/billiard/Kinect.java
+++ src/main/java/pl/ncdc/billiard/Kinect.java
@@ -60,6 +60,34 @@ public class Kinect extends J4KSDK {
 	public void onSkeletonFrameEvent(boolean[] skeleton_tracked, float[] positions, float[] orientations,
 			byte[] joint_status) {
 	}
+	
+	public static Point whiteBallDetection(Mat fullPicture, Mat circles, int xCut, int yCut, int width, int height, int radius)
+	{
+		double maxSum = Integer.MIN_VALUE;
+		Point whiteBallPoint = new Point(null);
+		
+		for (int i = 0; i < circles.cols(); i++) {
+			double[] c = circles.get(0, i);
+			Point center = new Point(c[0] + xCut, c[1] + yCut);
+			Rect rect = new Rect((int) center.x - radius, (int) center.y - radius, radius * 2, radius * 2);
+			
+			double rectSum = 0;
+			for (int j = rect.y; j <= rect.y + rect.height; j++) {
+				for (int k = rect.x; k <= rect.x + rect.width; k++) {
+					rectSum += fullPicture.get(j, k)[0];
+					rectSum += fullPicture.get(j, k)[1];
+					rectSum += fullPicture.get(j, k)[2];
+					}
+			}
+			
+			if (rectSum > maxSum) {
+				whiteBallPoint = center;
+				maxSum = rectSum;
+			}	
+		}
+			
+		return whiteBallPoint;
+	}
 
 	@Override
 	public void onColorFrameEvent(byte[] data) {
@@ -79,16 +107,17 @@ public class Kinect extends J4KSDK {
 		List<Ball> list = new ArrayList<Ball>();
 		Ball ball;
 
-		for (int x = 0; x < circles.cols(); x++) {
+		Point whiteBallPoint = whiteBallDetection(frame, circles, x, y, w, h, 14);
+		ball = new Ball(0, whiteBallPoint);
+		table.setWhiteBall(ball);
+		
+		for (int x = 1; x < circles.cols(); x++) {
 			double[] c = circles.get(0, x);
 			Point point = new Point();
 			point.x = w - c[0];
 			point.y = c[1];
 			ball = new Ball(x, point);
-			if (x == 0)
-				table.setWhiteBall(ball);
-			else
-				list.add(ball);
+			list.add(ball);
 		}
 		int i=1;
 		Collections.sort(list, new CustomComparator());
