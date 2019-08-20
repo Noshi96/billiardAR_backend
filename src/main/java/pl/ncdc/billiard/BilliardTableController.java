512d95490063b0034d3fc1e6fb26ec6e8199bc8b
Noshi96 <2spawel2@gmail.com>
2019-08-20 09:18:59 +0200

    updated utf8

--- src/main/java/pl/ncdc/billiard/BilliardTableController.java
+++ src/main/java/pl/ncdc/billiard/BilliardTableController.java
@@ -59,8 +59,6 @@ public class BilliardTableController {
 		tableService.selectBall(ballId);
 	}
 
-    @Autowired
-    IndividualTrainingService individualTrainingService;
 
 	@PutMapping("/pocket/{pocketId}")
 	public void selectPocket(@PathVariable Long pocketId) {
@@ -113,45 +111,6 @@ public class BilliardTableController {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 		return points;
 	}   
-    
-    @RequestMapping(value = "/updateIndividualTraining/{id}", method = RequestMethod.POST)
-    public IndividualTraining updateIndividualTraining(@RequestBody IndividualTrainingCommand individualTrainingCommand, @PathVariable Long id){
-        return individualTrainingService.updateIndividualTraining(individualTrainingCommand, id);
-    }
-    
-    @RequestMapping(value = "/getIndividualTrainingsByLvl/{lvl}", method = RequestMethod.GET)
-    public List<IndividualTraining> getArticleById(@PathVariable String lvl){
-        return individualTrainingService.sortListByLvl(lvl);
-    }
-        
-	@RequestMapping(method = RequestMethod.GET, path = "/fetchTreningById/{id}")
-	public ResponseEntity<IndividualTraining> fetch(@PathVariable Long id) {
-		IndividualTraining individualTraining = individualTrainingService.fetch(id);
-		if(individualTraining == null) {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-		return new ResponseEntity<IndividualTraining>(individualTraining, HttpStatus.OK);
-	}
-	
-	@RequestMapping(method  = RequestMethod.GET, path="/fetchAll")
-	public List<IndividualTraining> listPerson(){
-		return individualTrainingService.fetchAll();
-	}
-		
-    @RequestMapping(value = "/getTreningAllInfoById/{id}", method = RequestMethod.GET)
-    public List<IndividualTrainingCommand> returnIndividualTrainingCommand(@PathVariable long id){
-        return individualTrainingService.returnIndividualTrainingCommand(id);
-    }
-    
-	@RequestMapping(method = RequestMethod.POST, path="/save")
-	public IndividualTraining save(@RequestBody IndividualTrainingCommand individualTrainingCommand) {
-		return individualTrainingService.save(individualTrainingCommand);
-	}
-	
-	@RequestMapping(method = RequestMethod.DELETE, path="/deleteIndividualTrenig/{id}")
-	public IndividualTraining delete(@PathVariable Long id) {
-		return individualTrainingService.delete(id);
-	}
-    
+      
 }
 
