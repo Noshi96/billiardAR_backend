52c9a0f92251fcea7142a6b4d8949108526cb2fc
rafal18985 <rafal18985@gmail.com>
2019-08-21 09:11:53 +0200

    Add more calibration params and reset.

--- src/main/java/pl/ncdc/billiard/controllers/CalibrationController.java
+++ src/main/java/pl/ncdc/billiard/controllers/CalibrationController.java
@@ -36,4 +36,11 @@ public class CalibrationController {
 		calibrationParams = calibrationService.save(calibrationParams);
 		simpMessagingTemplate.convertAndSend("/calibration/live", calibrationParams);
 	}
+
+	@GetMapping("/reset")
+	public CalibrationParams resetToDefault() {
+		CalibrationParams calibrationParams = this.calibrationService.resetToDefault();
+		simpMessagingTemplate.convertAndSend("/calibration/live", calibrationParams);
+		return calibrationParams;
+	}
 }
